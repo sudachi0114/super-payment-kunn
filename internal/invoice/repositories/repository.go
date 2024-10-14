a2ba9f76122105e1c3a78c8f70fee9c4510630aa
sudachi0114 <jnevariste@gmail.com>
2024-10-14 16:53:22 +0900

    Memory でデータを保持する実装と RDS とやりとりする実装を分けて作成

--- internal/invoice/repositories/repository.go
+++ internal/invoice/repositories/repository.go
@@ -1,37 +1,77 @@
 package repositories
 
 import (
+	"database/sql"
 	"errors"
+	"log"
 	"super-payment-kunn/models"
 )
 
 type InvoiceRepository interface {
-	SelectAll() []*models.Invoice
+	SelectAll() ([]*models.Invoice, error)
 	Store(invoice *models.Invoice) error
 }
 
 var dbOnMemory = make(map[int]*models.Invoice, 0)
 
-type invoiceRepository struct {
+type invoiceRepositoryOnMemory struct {
 	db map[int]*models.Invoice
 }
 
-func NewInvoiceRepository() InvoiceRepository {
-	return &invoiceRepository{db: dbOnMemory}
+func NewInvoiceRepositoryOnMemory() InvoiceRepository {
+	return &invoiceRepositoryOnMemory{db: dbOnMemory}
 }
 
-func (r *invoiceRepository) SelectAll() []*models.Invoice {
+func (r *invoiceRepositoryOnMemory) SelectAll() ([]*models.Invoice, error) {
 	invoices := make([]*models.Invoice, 0, len(r.db))
 	for _, invoice := range r.db {
 		invoices = append(invoices, invoice)
 	}
-	return invoices
+	return invoices, nil
 }
 
-func (r *invoiceRepository) Store(invoice *models.Invoice) error {
+func (r *invoiceRepositoryOnMemory) Store(invoice *models.Invoice) error {
 	if _, exists := r.db[invoice.ID]; exists {
 		return errors.New("invoice ID is already exists")
 	}
 	r.db[invoice.ID] = invoice
+	return nil
+}
+
+type invoiceRepositoryOnRDB struct {
+	db *sql.DB
+}
+
+func NewInvoiceRepositoryOnRDB(db *sql.DB) InvoiceRepository {
+	return &invoiceRepositoryOnRDB{db: db}
+}
+
+func (r *invoiceRepositoryOnRDB) SelectAll() ([]*models.Invoice, error) {
+	query := "SELECT id, amount, due_date, issue_date, total_amount, fee_rate, tax_rate, status FROM invoice"
+	rows, err := r.db.Query(query)
+	if err != nil {
+		log.Fatalf("Error retriving invoice: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var invoices []*models.Invoice
+	for rows.Next() {
+		var invoice models.Invoice
+		if err := rows.Scan(&invoice.ID, &invoice.Amount, &invoice.DueDate, &invoice.IssueDate, &invoice.TotalAmount, &invoice.FeeRate, &invoice.TaxRate, &invoice.Status); err != nil {
+			log.Fatalf("Error scaning invoice: %v", err)
+		}
+		invoices = append(invoices, &invoice)
+	}
+
+	return invoices, nil
+}
+
+func (r *invoiceRepositoryOnRDB) Store(invoice *models.Invoice) error {
+	query := "INSERT INTO invoice (amount, due_date, issue_date, total_amount, fee_rate, tax_rate, status) VALUES (?, ?, ?, ?, ?, ?, ?)"
+	_, err := r.db.Exec(query, invoice.Amount, invoice.DueDate, invoice.IssueDate, invoice.TotalAmount, invoice.FeeRate, invoice.TaxRate, invoice.Status)
+	if err != nil {
+		log.Fatalf("Error Saving invoice: %v", err)
+	}
 	return nil
 }
