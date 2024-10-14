7211b796e66836d8cadd3ca19e0951ccfe2e3e0c
sudachi0114 <jnevariste@gmail.com>
2024-10-14 16:09:45 +0900

    リクエストされる場合のモデルと、永続化するモデルは異なるものであると気づいたので分けて定義する

--- models/invoice.go
+++ models/invoice.go
@@ -2,16 +2,28 @@ package models
 
 import "time"
 
+type InvoiceRequestJson struct {
+	Amount  int `json:"amount"`
+	DueDate int `json:"due_date"`
+}
+
+type InvoiceStatus string
+
+const (
+	StatusUnprocessed InvoiceStatus = "未処理"
+	StatusProcessing  InvoiceStatus = "処理中"
+	StatusPaid        InvoiceStatus = "支払い済み"
+	StatusError       InvoiceStatus = "エラー"
+)
+
 // 請求書データ (企業・取引先に紐づく)
 type Invoice struct {
-	ID      int       `json:"id"`
-	Amount  int       `json:"amount"`   // 支払金額
-	DueDate time.Time `json:"due_date"` // 支払期日
-	// 発行日
-	// 請求金額
-	// 手数料
-	// 手数料率
-	// 消費税
-	// 消費税率
-	// ステータス (未処理、処理中、支払い済み、エラー)
+	ID          int
+	Amount      int           // 支払金額
+	DueDate     time.Time     // 支払期日
+	IssueDate   time.Time     // 発行日
+	TotalAmount float64       // 請求金額
+	FeeRate     float32       // 手数料率 -> 手数料
+	TaxRate     float32       // 消費税率 → 消費税
+	Status      InvoiceStatus // ステータス (未処理、処理中、支払い済み、エラー)
 }
