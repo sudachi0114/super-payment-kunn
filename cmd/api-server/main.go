ab2f7970eb4f6cc2bd26bf249639f64321d9a589
sudachi0114 <jnevariste@gmail.com>
2024-10-14 15:20:59 +0900

    main でインスタンスを作って依存関係を注入

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"net/http"
 	"super-payment-kunn/internal/invoice/handler"
+	"super-payment-kunn/internal/invoice/repositories"
+	"super-payment-kunn/internal/invoice/service"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	apiInvoiceHandler := handler.NewInvoiceHandler()
+	invoiceRepo := repositories.NewInvoiceRepository()
+	invoiceService := service.NewInvoiceService(invoiceRepo)
+	apiInvoiceHandler := handler.NewInvoiceHandler(invoiceService)
 
 	mux.HandleFunc("/api/invoices", apiInvoiceHandler.HandleApiInvoice)
 
