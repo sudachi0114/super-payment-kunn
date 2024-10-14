79f4de1f977b79b2e0d93c2b032c0d8f2663630c
sudachi0114 <jnevariste@gmail.com>
2024-10-14 14:41:26 +0900

    REST API で提供するエンドポイントを定義

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func handleApiInvoice(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "GET /api/invoices is not implemented")
+		return
+	case http.MethodPost:
+		fmt.Fprintln(w, "POST /api/invoices is not implemented")
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed")
+		return
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/invoices", handleApiInvoice)
 
 	log.Println("Starting web server on :8080")
 	http.ListenAndServe(":8080", mux)
